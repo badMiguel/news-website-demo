8bb9579976f1276b99f9c7c57b1fdcf647541a10
Hyein Lee <hyein9792@gmail.com>
2025-05-01 01:11:11 +0930

    fill in empty query

--- lib/model/db.php
+++ lib/model/db.php
@@ -91,11 +91,15 @@ class Model
     public function updateNewsInDB(int $newsId): void
     {
         try {
+            session_start();
+            $authorId = $_SESSION['user_id'];
+            session_write_close();
+
             $statement = $this->db->prepare("
-                UPDATE news 
-                SET news_title = :title, news_summary = :summary, body = :body, edited_date = CURRENT_TIMESTAMP
-                WHERE news_id = :newsId
+                INSERT INTO news (news_title, news_summary, body, author_id, created_date, edited_date)
+                VALUES (:title, :summary, :body, :authorId, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)
             ");
+            
             $statement->execute([
                 "title" => $_POST["news_title"],
                 "summary" => $_POST["news_summary"],
