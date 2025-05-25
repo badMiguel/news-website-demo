badaa0014572949b3aa51f234539affd1b5b79d5
miguel <badayosbiboy@gmail.com>
2025-05-25 15:03:18 +0930

    styles on edit news

--- lib/views/edit_news.php
+++ lib/views/edit_news.php
@@ -12,37 +12,51 @@ if (isset($_SESSION['newsEditStatus'])) {
 session_write_close();
 ?>
 
-<form method="POST" action="/news/edit/submit" enctype="multipart/form-data">
+<form class="edit-form" method="POST" action="/news/edit/submit" enctype="multipart/form-data">
     <input type="hidden" name="news_id" value="<?php echo htmlspecialchars($newsDetails[0]['news_id']); ?>">
 
-    <label for="news_title">Title: </label>
-    <input type="text" name="news_title" value="<?php echo htmlspecialchars($newsDetails[0]['news_title']); ?>" required>
+    <div class="edit-form--field">
+        <label class="edit-form--label" for="news_title">Title</label>
+        <input class="edit-form--input" type="text" name="news_title" value="<?php echo htmlspecialchars($newsDetails[0]['news_title']); ?>" required>
+    </div>
 
-    <label for="news_subtitle">Summary: </label>
-    <textarea name="news_subtitle" required><?php echo htmlspecialchars($newsDetails[0]['news_subtitle']); ?></textarea>
+    <div class="edit-form--field">
+        <label class="edit-form--label" for="news_subtitle">Summary</label>
+        <textarea class="edit-form--input" name="news_subtitle" required rows="5"><?php echo htmlspecialchars($newsDetails[0]['news_subtitle']); ?></textarea>
+    </div>
 
-    <label for="body">Body: </label>
-    <textarea name="body" required><?php echo htmlspecialchars($newsDetails[0]['body']); ?></textarea>
+    <div class="edit-form--field">
+        <label class="edit-form--label" for="body">Body</label>
+        <textarea class="edit-form--input" name="body" required rows="20"><?php echo htmlspecialchars($newsDetails[0]['body']); ?></textarea>
+    </div>
 
-    <?php if (htmlspecialchars($newsDetails[0]["image_path"])): ?>
-        <img src="/../images/<?= htmlspecialchars($newsDetails[0]['image_path']) ?>" />
-    <?php endif ?>
-    <label for="image">Select Image:</label>
-    <input type="file" name="image" accept="image/*" />
+    <div class="edit-form--field">
+        <p class="edit-form--label">Category</p>
+        <div class="edit-form--category-list">
+            <?php foreach ($categoryList as $c): ?>
+                <label>
+                    <input
+                        type="checkbox"
+                        name="category[]"
+                        <?php if (in_array($c["category"], $newsDetails[0]['category'])): ?>
+                        checked
+                        <?php endif ?>
+                        value="<?= htmlspecialchars(lcfirst($c["category_id"])) ?>" />
+                    <?= $c["category"] ?>
+                </label>
+            <?php endforeach ?>
+        </div>
+    </div>
 
-    <p>Category:</p>
-    <?php foreach ($categoryList as $c): ?>
-        <label>
-            <input
-                type="checkbox"
-                name="category[]"
-                <?php if (in_array($c["category"], $newsDetails[0]['category'])): ?>
-                checked
-                <?php endif ?>
-                value="<?= htmlspecialchars(lcfirst($c["category_id"])) ?>" />
-            <?= $c["category"] ?>
-        </label>
-    <?php endforeach ?>
+    <div class="edit-form--field">
+        <label class="edit-form--label" for="image">Image</label>
+        <input class="edit-form--input" type="file" name="image" accept="image/*" />
+    </div>
+    <!-- <?php if (htmlspecialchars($newsDetails[0]["image_path"])): ?> -->
+    <!--     <img style="width: 100%;" src="/../images/<?= htmlspecialchars($newsDetails[0]['image_path']) ?>" /> -->
+    <!-- <?php endif ?> -->
 
-    <button type="submit">Update News</button>
+    <div class="edit-form--submit--container">
+    <button class="edit-form--submit" type="submit">Update News</button>
+    </div>
 </form>
